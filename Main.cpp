4d82ed6cfae13d604bdd56d17b11c1312e944a85
Jimmie Bergmann <jimmiebergmann@gmail.com>
2019-11-17 17:49:02 +0100

    Initial test.

---
+++ Main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    int a = 1 + 2;
+    std::cout << a << std::endl;
+    std::cout << a * 2 << std::endl;
+    std::cout << a / 2 << std::endl;
+
+    return 0;
+}
---
+++ Test/MyTest.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf ("Test: %c\n", 'a');
+
+    return 0;
+}
